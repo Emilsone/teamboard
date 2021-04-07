61d8e192fbceccd5e5d52ba9f5fe2e0f2f45a5d8
Emilsone <lawsonezekiel68@gmail.com>
2021-04-07 01:54:17 -0700

    delete footer page

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen  font-roboto">
+  <div class="flex h-screen font-roboto">
     <SideBar />
 
     <div class="flex-1 flex flex-col overflow-hidden">
@@ -7,29 +7,24 @@
 
       <main class="flex-1 overflow-x-hidden overflow-y-auto bg-back">
         <div class="container mx-auto px-6 py-8">
-         <router-view />
+          <router-view />
         </div>
       </main>
-      <footer>
-      <Footer />
-	</footer>
     </div>
   </div>
 </template>
 
 <script>
-import Nav from "./nav/nav.vue"
-import Footer from "@/components/Footer.vue"
+import Nav from "./nav/nav.vue";
 import SideBar from "./side-bar/side-bar.vue";
 export default {
   name: "Dashboard",
-  components: { SideBar, Nav,Footer },
+  components: { SideBar, Nav, },
 };
 </script>
 
 <style scoped>
-.bg-back{
-background:#F0F4F6,
-
+.bg-back {
+  background: #f0f4f6;
 }
 </style>
